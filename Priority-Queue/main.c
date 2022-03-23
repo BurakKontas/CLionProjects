37e7ddb7d8386022c255242bd837fc160058bdbf
BurakKontas <aburakkontas@trakya.edu.tr>
2022-03-23 13:32:13 +0300

    Menu view created queues list edited

--- Priority-Queue/main.c
+++ Priority-Queue/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+//some type of const usage
 #define MAXP 7//max priority
+#define MAXM 12//menu max
 
 //Priority queue data structure with linked lists
 //every priority level has their own queue
@@ -15,8 +18,8 @@
 //LList = Linked List
 
 struct UserData{
-    char priority;//smaller than integer
-    int no;
+    unsigned char priority;//smaller than integer
+    unsigned int no;
     char name[20];
     char surname[20];
     char address[50];
@@ -31,17 +34,43 @@ struct LinkedList{
 
 typedef struct LinkedList LList;
 
-void create(){
-
-}
-
 LList* memory(){
     return (LList*)malloc(sizeof(LList));
 }
 
 int main() {
     //I'll store all queue starting addresses in this array
-    int *queues = (int*)malloc(MAXP*sizeof(int));
+    LList* queues[MAXP] = {memory(),memory(),memory(),memory(),memory(),memory(),memory()};//starting addresses
+    /*
+     * LList* queues[MAXP];
+     * for(int i = 0,i<MAXP,++i) {
+     *      queues[i] = memory();
+     * }
+    */
+    const char MENU[][MAXM] = {"--MENU--","Add(Push)","Remove(Pop)","Insert","Show","Quit"};//unchangeable
+    while(1) {
+        for (int i = 0; i < sizeof(MENU) / MAXM; ++i) {
+            if (i == 0) printf("%s\n", MENU[i]);
+            else printf("%d-) %s\n", i, MENU[i]);
+        }
+        printf("Selection: ");
+        int selection;
+        scanf("%d",&selection);
+        if(selection == 1){
 
+        }
+        else if(selection == 2){
+
+        }
+        else if(selection == 3){
+
+        }
+        else if(selection == 4){
+
+        }
+        else if(selection == 5){
+
+        }
+    }
     return 0;
 }
