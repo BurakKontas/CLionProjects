0f039b3acde401aa056aefa84ca59c64e08e8f6f
BurakKontas <aburakkontas@trakya.edu.tr>
2022-03-23 12:51:39 +0300

    Program operation structure and function explained

--- Priority-Queue/main.c
+++ Priority-Queue/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+
+//Priority queue data structure with linked lists
+//every priority level has their own queue
+//when showing we should keep max priority level
+//showing function works like:
+//  start from the highest priority's list and print them in order
+//  if the highest priority level queue is empty increase "int highest_level" variable (--highest_level)
+//TODO: functions: push,pop,show,edit(edits data's priority level)
+
+
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
